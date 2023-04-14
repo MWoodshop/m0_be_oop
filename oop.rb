717dda147b42b75899e3109ae938e7724ae1b90c
Mike Wood <mikewwood@icloud.com>
2023-04-13 20:35:38 -0400

    include completed assignments.

--- oop.rb
+++ oop.rb
@@ -2,15 +2,50 @@
 # it should have a dynamic name attribute
 # it should have a color attribute, that is silver by default
 # it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
+class Unicorn
+  attr_reader :name, :color
 
+  def initialize(name, color = 'silver')
+    @name = name
+    @color = color
+  end
 
+  def say(message)
+    "*~*#{message}*~*"
+  end
+end
+
+unicorn1 = Unicorn.new('Sparkles', 'Rainbow')
+p unicorn1
+
+puts unicorn1.say('I am a unicorn')
 
 #  Write a class called Vampire
 #  it should have a dynamic name attribute
 #  it should have a pet attribute, that is a bat, by default BUT it could be dynamic if info is passed in initially
 #  it should have a thirsty attribute, that is true by default
 #  it should have a drink method. When called, the thirsty attribute changes to false
+p ''
 
+class Vampire
+  attr_reader :name, :thirsty, :pet
+
+  def initialize(name, pet = 'bat')
+    @name = name
+    @thirsty = true
+    @pet = pet
+  end
+
+  def drink
+    @thirsty = false
+  end
+end
+
+vlad = Vampire.new('Vlad', true)
+p vlad
+
+vlad.drink
+p vlad
 
 #  Write a Dragon class
 #  it should have a dynamic name attribute (string)
@@ -19,6 +54,42 @@
 #  it should have a is_hungry attribute that is true by default
 #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
 
+class Dragon
+  attr_reader :name, :rider, :color, :is_hungry, :times_eaten
+
+  def initialize(name, rider, color, is_hungry = true)
+    @name = name
+    @rider = rider
+    @color = color
+    @is_hungry = is_hungry
+    @times_eaten = 0
+  end
+
+  def eat
+    @times_eaten += 1
+    return unless @times_eaten >= 4
+
+    @is_hungry = false
+  end
+end
+
+smaug = Dragon.new('Smaug', 'Frodo', 'Red')
+p smaug
+
+smaug.eat
+p smaug.is_hungry
+
+smaug.eat
+p smaug.is_hungry
+
+smaug.eat
+p smaug.is_hungry
+
+smaug.eat
+p smaug.is_hungry
+
+smaug.eat
+p smaug.is_hungry
 
 #  Write a Hobbit class
 #  it should have a dynamic name attribute (string)
@@ -28,3 +99,51 @@
 #  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 #  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
 #  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+
+p ''
+class Hobbit
+  def initialize(name, disposition, age = 0, is_adult = false, is_old = false)
+    @name = name
+    @disposition = disposition
+    @age = age
+    @is_adult = is_adult
+    @is_adult = true if @age >= 33
+    @is_old = is_old
+    return unless @name == 'Frodo'
+
+    @has_ring = true
+  end
+
+  def celebrate_birthday
+    @age += 1
+    if @age >= 101
+      @is_old = true
+    elsif @age >= 33
+      @is_adult = true
+    end
+  end
+
+  def has_ring
+    @name == 'Frodo'
+  end
+end
+
+bilbo = Hobbit.new('Bilbo', 'Adventurous', 100)
+p bilbo
+
+bilbo.celebrate_birthday
+p bilbo
+
+bilbo.celebrate_birthday
+p bilbo
+
+p ''
+
+frodo = Hobbit.new('Frodo', 'Brave', 31)
+p frodo
+
+frodo.celebrate_birthday
+p frodo
+
+frodo.celebrate_birthday
+p frodo
--- self-eval.md
+++ self-eval.md
@@ -1,5 +1,13 @@
 # Self-Evaluation
 
 - This final CFU encompassed everything you've learned in Mod 0, and maybe even required some learning outside of what you've explicitly been taught. How did it feel as you faced these challenges?
+
+It started to become a bit overwhelming when trying to include everything and figure out the correct syntax for all of the different things we've learned.
+
 - How is your workflow going, in terms of taking breaks, staying focused, etc.?
+
+The amount of work is about at the maximum I think I could reasonably do while still maintaining a healthy study/life balance.
+
 - What did you learn from this exercise that you want to remember as you begin Mod 1?
+
+How the classes work along with all of the other things we've learned.
